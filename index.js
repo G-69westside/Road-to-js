5962dd25abcd0d25170b7cf89bff0aed7a0d64b1
Tony Maluki <tonymaluki@gmail.com>
2021-12-20 16:31:14 +0300

    Implementing Arrow functions

--- index.js
+++ index.js
@@ -507,21 +507,26 @@
 //   console.log(item);
 // });
 
-
-
-
 //Changing Text and HTML Content
-let books = document.querySelectorAll("#book-list li .name");
-Array.from(books).forEach(function (book) {
-  book.textContent += "test";
-  //This code must run from this loop
-  //Also, we can append (write next to) the already existing text
-  // and this can be done by book.textContent += "(test)";
-});
-//This literally grabs the textcontent
-//As you can see, we have changed variable books to an array but that is not neccessary, we can still work with it without changing into an array since the output will be a nodeList
+// let books = document.querySelectorAll("#book-list li .name");
+// Array.from(books).forEach(function (book) {
+//   book.textContent += "test";
+//   //This code must run from this loop
+//   //Also, we can append (write next to) the already existing text
+//   // and this can be done by book.textContent += "(test)";
+// });
+// //This literally grabs the textcontent
+// //As you can see, we have changed variable books to an array but that is not neccessary, we can still work with it without changing into an array since the output will be a nodeList
 
-const bookList = document.querySelector("#book-list");
-console.log(bookList.innerHTML);
-bookList.innerHTML = "<h1>Books and more books</h1>";
-bookList.innerHTML += "<p>This is how you add HTML </P>";
+// const bookList = document.querySelector("#book-list");
+// console.log(bookList.innerHTML);
+// bookList.innerHTML = "<h1>Books and more books</h1>";
+// bookList.innerHTML += "<p>This is how you add HTML </P>";
+
+//Now you can also use arrow Functions to make it shorter;
+//Have a look at it below:
+let book = document.querySelectorAll("#book-list li .name");
+
+book.forEach((books) => {
+  books.textContent = "test";
+});
