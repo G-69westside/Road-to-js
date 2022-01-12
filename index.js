15970769415a0dace934d0e2602a5ce6af840ce7
Tony Maluki <tonymaluki@gmail.com>
2022-01-12 22:38:59 +0300

    event bubbling

--- index.js
+++ index.js
@@ -593,8 +593,10 @@
 
 //Event Bubbling
 
-
-
-const list  =document.querySelector('#book-list ul')
-list.addEventListener('cl')
-
+const list = document.querySelector("#book-list ul");
+list.addEventListener("click", function (e) {
+  if (e.target.className == "delete") {
+    const li = e.target.parentElement;
+    li.remove();
+  }
+});
