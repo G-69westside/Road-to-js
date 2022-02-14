1e817b3cddca0e28f5ffe4da59dca94710ef0a60
Tony Maluki <tonymaluki@gmail.com>
2022-02-15 00:55:09 +0300

    Road to Glory

--- draft.js
+++ draft.js
@@ -208,6 +208,11 @@ const numbers = [1, 2, 3];
 // for (let number of numbers) {
 //   console.log(number);
 // }
-
-const joined = numbers.join();
+const joined = numbers.join("-");
 console.log(joined);
+
+const message = "This is my first message";
+const parts = message.split(" ");
+console.log(parts);
+const combined = parts.join("-");
+console.log(combined);
