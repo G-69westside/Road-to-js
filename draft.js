8baadeeb1acf994b2fa2250ff44ab19a05469683
Tony Ndeleva <tonymaluki@gmail.com>
2022-03-14 10:03:11 +0300

    moving elements

--- draft.js
+++ draft.js
@@ -331,6 +331,18 @@ const User = {
 // console.log(output);
 
 // // when indexOf a number is -1 , it means the number is not available
-const numbers = [1, 2, 3, 4];
-console.error("Invalid");
 
+const numbers = [1, 2, 3, 4];
+function move(array, index, offset) {
+  const position = index + offset;
+  if (position >= array.length || position < 0) {
+    console.error("invalid offset");
+    return;
+  }
+  const output = [...array];
+  const element = output.splice(index, 1)[0];
+  output.splice(index + offset, 0, element);
+  return output;
+}
+const output = move(numbers, 0, -3);
+console.log(output);
