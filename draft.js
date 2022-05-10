0850643906a0b1a32bad8843a00c150a14feee7a
Tony Maluki <tonymaluki@gmail.com>
2022-05-10 17:21:26 +0300

    Changing this in JS

--- draft.js
+++ draft.js
@@ -407,24 +407,35 @@ const User = {
 // console.log(interest1(10000));
 
 //Getters and Setters
-const person = {
-  firstName: "Tony",
-  lastName: "Maluki",
-  get fullName() {
-    return `${person.firstName} ${person.lastName}`;
-  },
-  set fullName(value) {
-    if (typeof value !== "string") throw new Error("Value is not a string");
-    const parts = value.split(" ");
-    if (parts.length !== 2) throw new Error("Enter a first and last name");
-    this.firstName = parts[0];
-    this.lastName = parts[1];
+// const person = {
+//   firstName: "Tony",
+//   lastName: "Maluki",
+//   get fullName() {
+//     return `${person.firstName} ${person.lastName}`;
+//   },
+//   set fullName(value) {
+//     if (typeof value !== "string") throw new Error("Value is not a string");
+//     const parts = value.split(" ");
+//     if (parts.length !== 2) throw new Error("Enter a first and last name");
+//     this.firstName = parts[0];
+//     this.lastName = parts[1];
+//   },
+// };
+// try {
+//   person.fullName = null;
+// } catch (e) {
+//   alert(e);
+// }
+
+// console.log(person);
+
+const video = {
+  title: "a",
+  tags: ["a", "b", "c"],
+  showTags() {
+    this.tags.forEach((tag) => {
+      console.log(this.title, tag);
+    });
   },
 };
-try {
-  person.fullName = null;
-} catch (e) {
-  alert(e);
-}
-
-console.log(person);
+video.showTags();
